3018d95823b9f26670bd438d1e484816f3ac7d43
01792166684 <tanzinatania491991@gmail.com>
2022-08-23 01:53:41 +0600

    fourth commit

--- images/java.js
+++ images/java.js
@@ -17,31 +17,56 @@
           const parentNode = document.getElementById('select-five');
           const li =document.createElement('li');
           li.innerText = playerValue ;
-       return   parentNode.appendChild(li);
-         
-  }
-  
+          return   parentNode.appendChild(li);}
+         //  function getElementById(){const parentNode = document.getElementById('select-five');
+         //  if(parentNode.childNodes.length>5){(parentNode.removeChild(parentNode.lastElementChild)) && (document.getElementById('btn-dusan').disabled =false );
+         //  return}};
+         //  function getElementValueById(){const parentNode = document.getElementById('select-five');
+         //  parentNode.removeChild(parentNode.lastElementChild);return};
+          
     document.getElementById('btn-dusan').addEventListener('click',function() {     
-  const player1 = getInputValueById('player-dusan'); 
-  const x =document.getElementById('btn-dusan');
-  x.disabled = true ;})
+  const player1 = getInputValueById('player-dusan');
+  const parentNode = document.getElementById('select-five');
+  if(parentNode.childNodes.length>5){(parentNode.removeChild(parentNode.lastElementChild)) && (document.getElementById('btn-dusan').disabled =false )}
+else{document.getElementById('btn-dusan').disabled=true;
+}
+
+})
   document.getElementById('btn-jamie').addEventListener('click',function() {     
-   const player1 = getInputValueById('player-jamie'); 
-   const x =document.getElementById('btn-jamie');
-   x.disabled = true ;})
+   const player1 = getInputValueById('player-jamie');
+   const parentNode = document.getElementById('select-five');
+  if(parentNode.childNodes.length>5){(parentNode.removeChild(parentNode.lastElementChild)) && (document.getElementById('btn-jamie').disabled =false )}
+else{document.getElementById('btn-jamie').disabled=true;
+}
+})
    document.getElementById('btn-karim').addEventListener('click',function() {     
       const player1 = getInputValueById('player-karim'); 
-      const x =document.getElementById('btn-karim');
-      x.disabled = true ;})
+      const parentNode = document.getElementById('select-five');
+  if(parentNode.childNodes.length>5){(parentNode.removeChild(parentNode.lastElementChild)) && (document.getElementById('btn-karim').disabled =false )}
+else{document.getElementById('btn-karim').disabled=true;
+}
+})
       document.getElementById('btn-kyllian').addEventListener('click',function() {     
          const player1 = getInputValueById('player-kyllian'); 
-         const x =document.getElementById('btn-kyllian');
-         x.disabled = true ;})
+         const parentNode = document.getElementById('select-five');
+  if(parentNode.childNodes.length>5){(parentNode.removeChild(parentNode.lastElementChild)) && (document.getElementById('btn-kyllian').disabled =false )}
+else{document.getElementById('btn-kyllian').disabled=true;
+}
+
+         })
          document.getElementById('btn-luis').addEventListener('click',function() {     
             const player1 = getInputValueById('player-luis'); 
-            const x =document.getElementById('btn-luis');
-            x.disabled = true ;})
+            const parentNode = document.getElementById('select-five');
+  if(parentNode.childNodes.length>5){(parentNode.removeChild(parentNode.lastElementChild)) && (document.getElementById('btn-luis').disabled =false )}
+else{document.getElementById('btn-luis').disabled=true;
+}
+})
             document.getElementById('btn-depay').addEventListener('click',function() {     
                const player1 = getInputValueById('player-depay'); 
-               const x =document.getElementById('btn-depay');
-               x.disabled = true ;})
+               const parentNode = document.getElementById('select-five');
+  if(parentNode.childNodes.length>5){(parentNode.removeChild(parentNode.lastElementChild))
+    && (document.getElementById('btn-depay').disabled =false )}
+else{(document.getElementById('btn-depay').disabled=true) 
+}
+})
+               // const remove = getElementValueById;
